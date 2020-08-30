867a26f67646929565a42dcf99fbb0c9f108a3f6
kamkyi <waihynhtun1994@gmail.com>
2020-08-30 15:03:05 +0630

    Update Lravel

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -96,11 +96,6 @@
                 <div class="title m-b-md">
                     <example-component></example-component>
                 </div><!--title-->
-
-                <div class="links">
-                    <a href="http://laravel-boilerplate.com" target="_blank"><i class="fa fa-book"></i> @lang('Docs')</a>
-                    <a href="https://github.com/rappasoft/laravel-boilerplate" target="_blank"><i class="fab fa-github"></i> GitHub</a>
-                </div><!--links-->
             </div><!--content-->
         </div><!--app-->
 
